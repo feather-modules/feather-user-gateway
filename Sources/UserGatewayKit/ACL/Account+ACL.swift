6fb0be009804260d630b9e828182e3b0e2adc58c
László Kovács <atomantinator@gmail.com>
2024-08-28 07:44:25 +0200

    added ACLs

--- Sources/UserGatewayKit/ACL/Account+ACL.swift
+++ Sources/UserGatewayKit/ACL/Account+ACL.swift
@@ -18,6 +18,22 @@ extension UserGateway.Account {
 
     public enum ACL: ACLSet {
 
-        public static var all: [Permission] = []
+        public static let list: Permission = .userAccount(.list)
+        public static let meDetail: Permission = .userAccount(
+            .custom("me_detail")
+        )
+        public static let meUpdate: Permission = .userAccount(
+            .custom("me_update")
+        )
+        public static let mePatch: Permission = .userAccount(
+            .custom("me_patch")
+        )
+
+        public static var all: [Permission] = [
+            list.self,
+            meDetail.self,
+            meUpdate.self,
+            mePatch.self,
+        ]
     }
 }
