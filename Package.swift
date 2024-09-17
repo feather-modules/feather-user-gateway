adf22f3da3c73595989b930c5b2ab9b5967145b1
GErP83 <gurrka@gmail.com>
2024-09-17 16:40:41 +0200

    add oauth module

--- Package.swift
+++ Package.swift
@@ -34,7 +34,9 @@ let package = Package(
         
         .package(url: "https://github.com/feather-modules/feather-user-api", .upToNextMinor(from: "0.8.2")),
         .package(url: "https://github.com/feather-modules/feather-oauth-api", .upToNextMinor(from: "0.1.3")),
-        .package(url: "https://github.com/feather-modules/feather-user-module", .upToNextMinor(from: "0.21.0")),
+        //.package(url: "https://github.com/feather-modules/feather-user-module", .upToNextMinor(from: "0.21.0")),
+        .package(path: "../feather-user-module"),
+        .package(path: "../feather-oauth-module"),
         .package(url: "https://github.com/feather-modules/feather-user-gateway-accounts-api", .upToNextMinor(from: "0.1.3")),
     ],
     targets: [
@@ -46,6 +48,7 @@ let package = Package(
                 .product(name: "OpenAPIRuntime", package: "swift-openapi-runtime"),
                 .target(name: "UserGatewayAccountsKit"),
                 .product(name: "UserModuleKit", package: "feather-user-module"),
+                .product(name: "OauthModuleKit", package: "feather-oauth-module"),
             ]
         ),
         .target(
@@ -62,6 +65,7 @@ let package = Package(
                 .product(name: "FeatherValidationFoundation", package: "feather-validation"),
                 .target(name: "UserGatewayDatabaseKit"),
                 .product(name: "UserModuleKit", package: "feather-user-module"),
+                .product(name: "OauthModuleKit", package: "feather-oauth-module"),
             ]
         ),
 
